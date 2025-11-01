5e5c60019b66a893abbe304c2c96c24698539757
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-11-01 13:36:15 +0000

    example of class and object learning

---
+++ TUF/main.java
@@ -0,0 +1,34 @@
+
+class Employee {
+    public int salary;
+    public String employeeName;
+
+    public void setSalary(int sal) {
+        salary = sal;
+    }
+
+    public void setEmployeeName(String name) {
+        employeeName = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+}
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee();
+
+        emp1.setEmployeeName("Rahul");
+
+        emp1.setSalary(10000);
+
+        System.out.println("Salary of " + emp1.employeeName + " is " + emp1.getSalary());
+
+    }
+
+}
