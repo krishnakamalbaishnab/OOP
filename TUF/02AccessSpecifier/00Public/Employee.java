bed2890972e71cc2b760a56fe01f6ae839f36a6c
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-12-16 19:55:59 +0000

    access specifier

---
+++ TUF/02AccessSpecifier/00Public/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    public String name;
+
+    public void displayName() {
+        System.out.println("The employee name is : " + name);
+
+    }
+
+}
---
+++ TUF/02AccessSpecifier/00Public/Main.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class Main {
+
+    public static void main(String[] args){
+        Employee employee1 = new Employee();
+        employee1.name = "Hero";
+        employee1.displayName();
+    }
+    
+}
