2f0a8718566ea51aa9df309b6870c26b276a4ee7
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-12-17 08:35:55 +0000

    one shot

---
+++ oneshot/Main.java
@@ -0,0 +1,24 @@
+
+
+
+
+class Test{
+    public static void testThis() {
+        System.out.println("Hello in test!");
+    }
+
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args){
+
+        Test.testThis();
+
+    }
+    
+}
+
+//so basically when we use static keyword we do not need to create an object manually
